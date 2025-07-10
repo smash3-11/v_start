13aef3bbdc83b95da7d4df7b8cc79425944e420d
Sokhib <websoon010@gmail.com>
2025-07-10 10:14:44 +0500

    first commit

---
+++ layout/default.js
@@ -0,0 +1,27 @@
+// function renderHeader() {
+//     const header = document.createElement('header');
+//     header.className = 'navbar';
+
+//     header.innerHTML = `
+//   <nav>
+//     <a href="/Visa/index.html">Главная</a>
+//     <a href="/pages/wallets/index.html">Кошельки</a>
+//     <a href="index.html" class="active">Транзакции</a>
+//   </nav>
+//   <div class="left">
+//     <a href="home.html" class="user-email">alexadams@google.com</a>
+//     <img src="login.png" alt="">
+//   </div>
+//     `;
+//     // document.body.prepend(header);
+
+// }
+
+
+// export function renderLayout() {
+//     const body = document.querySelector('body');
+//     const header = renderHeader();
+
+//     body.prepend(header);
+
+// }
---
+++ main.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { defineConfig } from 'vite';
+// import { renderLayout } from './layouts/default.js';
+
+const pages = [
+    {
+        path: '/',
+        loadHtml: async (app) => {
+            const response = await axios.get('/index.html');
+            app.innerHTML = response.data;
+        },
+        loadStyles: async () => {
+            await import('/style/style.css');
+        },
+        loadScripts: async () => {
+           
+        }
+    },
+    {
+        path: '/wallets',
+        loadHtml: async (app) => {
+            const response = await axios.get('/pages/wallets/index.html');
+            app.innerHTML = response.data;
+        },
+        loadStyles: async () => {
+            await import('/style/style.css');
+            console.log("hello");
+            
+        },
+        loadScripts: async () => {
+         
+        }
+    },
+    {
+        path: '/transactions',
+        loadHtml: async (app) => {
+            const response = await axios.get('/pages/transactions/index.html');
+            app.innerHTML = response.data;
+        },
+        loadStyles: async () => {
+            await import('/style/style.css');
+        },
+        loadScripts: async () => {
+            
+        }
+    }
+];
+
+async function loadNotFoundPage(app) {
+    const response = await axios.get('/404.html'); 
+    await import('/style/style.css');
+    app.innerHTML = response.data;
+}
+
+async function router() {
+    const path = window.location.pathname;
+    const app = document.getElementById('app');
+
+    const page = pages.find(elem => elem.path === path);
+
+    if (!page) {
+        return await loadNotFoundPage(app);
+    }
+
+    await page.loadStyles();
+    await page.loadHtml(app);
+    await page.loadScripts();
+
+    renderLayout();
+}
+
+router();
